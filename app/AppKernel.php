603ef0b4d221c215498d3a513ae80bfc5c62167b
suresh shrestha <suresh.shrestha@cashonad.com>
2014-07-21 12:44:58 +0545

    User login page

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
 
             // Adding for FosUserBundle
             new FOS\UserBundle\FOSUserBundle(),
+            new Acme\UserBundle\AcmeUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
